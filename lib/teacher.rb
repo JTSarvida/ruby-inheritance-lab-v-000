b7dcbf47182c93f5bb92d97a42f80664867a4c3f
Joshua Tan <jtsarvida@gmail.com>
2018-05-19 16:40:49 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,5 +1,9 @@
 require_relative "./user.rb"
+<<<<<<< HEAD
 class Teacher < User
+=======
+class Teacher
+>>>>>>> e3cfe88dd1ac53d6134464fb6d8fd72c93937af6
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
   
